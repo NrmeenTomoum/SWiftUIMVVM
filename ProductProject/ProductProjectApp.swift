913a4529b5725f721797cb552eb53073a1534b3e
Nrmeen Tomoum <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2022-04-27 20:41:14 +0400

    Initial Commit

---
+++ ProductProject/ProductProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  ProductProjectApp.swift
+//  ProductProject
+//
+//  Created by Nrmeen Tomoum on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProductProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
